b39d1ce92a0fedfcad70a0c46b34101dc490afe0
Scott Chamberlain <myrmecocystus@gmail.com>
2018-04-30 13:33:57 -0700

    use markdown docs, fix #10 - not really fixed, but as good as we can do related, export a function fro user to get the all dataset

--- R/eol.R
+++ R/eol.R
@@ -19,6 +19,16 @@
 #' @param ... curl options passed on to \code{\link[crul]{HttpClient}}
 #'
 #' @details
+#' `eol_invasive_data()` gives you the entire data.frame from 
+#' the "dataset=all", while `eol()` let's you search on a vector of names 
+#' against any of the datasets
+#' 
+#' IMPORTANT: note that setting `dataset="all"` will give you surprising results. 
+#' EOL does not include informaiton on which of the invasive datasets (i.e., gisd100, 
+#' gisd, isc, daisie, i3n, or mineps) the taxon is found in, and sometimes e.g., if
+#' taxon X is in GISD, you might not find it in "all", weird. I don't know 
+#' why that's happening, but it shouldn't happen. 
+#' 
 #' IMPORTANT: When you get a returned NaN for a taxon, that means it's not on
 #' the invasive list in question. If the taxon is found, a taxon identifier
 #' is returned.
@@ -27,40 +37,37 @@
 #' pull down all data before we can search for your species. Note there is no
 #' parameter in this API method for searching by taxon name.
 #'
-#' This function is vectorized, so you can pass a single name or a vector
+#' `eol()` is vectorized, so you can pass a single name or a vector
 #' of names.
 #'
 #' It's possible to return JSON or XML with the EOL API. However, this function
-#' only returns JSON for now.
+#' only returns JSON.
 #'
 #' Options for the dataset parameter are
-#' \itemize{
-#'  \item all - All datasets
-#'  \item gisd100 - 100 of the World's Worst Invasive Alien Species
+#' 
+#' - all - All datasets
+#' - gisd100 - 100 of the World's Worst Invasive Alien Species
 #'  (Global Invasive Species Database) http://eol.org/collections/54500
-#'  \item gisd - Global Invasive Species Database 2013
+#' - gisd - Global Invasive Species Database 2013
 #'  http://eol.org/collections/54983
-#'  \item isc - Centre for Agriculture and Biosciences International Invasive
+#' - isc - Centre for Agriculture and Biosciences International Invasive
 #'  Species Compendium (ISC) http://eol.org/collections/55180
-#'  \item daisie - Delivering Alien Invasive Species Inventories for Europe
+#' - daisie - Delivering Alien Invasive Species Inventories for Europe
 #'  (DAISIE) Species List http://eol.org/collections/55179
-#'  \item i3n - IABIN Invasives Information Network (I3N) Species
+#' - i3n - IABIN Invasives Information Network (I3N) Species
 #'  http://eol.org/collections/55176
-#'  \item mineps - Marine Invaders of the NE Pacific
+#' - mineps - Marine Invaders of the NE Pacific
 #'  Species http://eol.org/collections/55331
-#' }
 #'
 #' Datasets are not updated that often. Here's last updated dates for some of
 #' the datasets as of 2014-08-25
 #'
-#' \itemize{
-#'  \item gisd100 updated 6 mos ago
-#'  \item gisd  updated 1 yr ago
-#'  \item isc updated 1 yr ago
-#'  \item daisie updated 1 yr ago
-#'  \item i3n updated 1 yr ago
-#'  \item mineps updated 1 yr ago
-#' }
+#' - gisd100 updated 6 mos ago
+#' - gisd  updated 1 yr ago
+#' - isc updated 1 yr ago
+#' - daisie updated 1 yr ago
+#' - i3n updated 1 yr ago
+#' - mineps updated 1 yr ago
 #'
 #' @return A list of data.frame's/strings with results, with each element
 #' named by the input elements to the name parameter.
@@ -163,3 +170,39 @@ getmatches <- function(x, y, z) {
     z[matched, ]
   }
 }
+
+#' @export
+#' @rdname eol
+eol_invasive_data <- function(...) {
+  args <- orc(list(per_page = 500, filter = 'taxa'))
+  path <- "/api/collections/1.0/55367.json"
+  cli <- crul::HttpClient$new(url = 'http://eol.org', 
+    opts = list(...))
+  tt <- cli$get(path, query = args)
+  tt$raise_for_status()
+  res <- jsonlite::fromJSON(tt$parse("UTF-8"), FALSE)
+  data_init <- res$collection_items
+  message(sprintf("Getting data for %s names...", res$total_items))
+
+  pages_get <- pages_left(res)
+
+  if (!is.null(pages_get)) {
+    out <- list()
+    for (i in pages_get) {
+      args <- orc(list(page = i, per_page = 500, filter = 'taxa'))
+      tt <- cli$get(path, query = args)
+      tt$raise_for_status()
+      res <- jsonlite::fromJSON(tt$parse("UTF-8"), FALSE)
+      out[[i]] <- res$collection_items
+    }
+    res2 <- orc(out)
+    dat_all <- do.call(c, list(data_init, do.call(c, res2)))
+    dat_all <- lapply(dat_all, "[", c("name", "object_id"))
+    dat <- todf(dat_all)
+  } else {
+    dat_all <- lapply(data_init, "[", c("name","object_id"))
+    dat <- todf(dat_all)
+  }
+  return(dat)
+}
+
